52d63bd64df7d487d3a2e001d5b1611f9ed4dc6f
Stephen Hollmann <87396185+shollmannr@users.noreply.github.com>
2021-08-20 09:47:07 -0400

    Final commit

--- hangman.js
+++ hangman.js
@@ -1,32 +1,30 @@
+// The word bank/answers.
 var programming_languages = [
 	"python",
 	"javascript",
-	"mongodb",
-	"json",
 	"java",
-	"html",
-	"css",
-	"c",
-	"csharp",
-	"golang",
-	"kotlin",
-	"php",
-	"sql",
-	"ruby"
+  "c",
+  "go",
+  "r",
+  "swift",
+  "php",
+  "html",
+  "kotlin",
+  "css"
 ]
-
-let answer = '';
+// Sets the max mistakes to 6, which is how many images I have.
+let answer = "";
 let maxWrong = 6;
 let mistakes = 0;
 let guessed = [];
 let wordStatus = null;
-
+// Selects a word randomly from the programming languages array
 function randomWord() {
   answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
 }
-
+// Generates the buttons/letters to click on to guess the answer.
 function generateButtons() {
-  let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+  let buttonsHTML = "abcdefghijklmnopqrstuvwxyz".split("").map(letter =>
     `
       <button
         class="btn btn-lg btn-primary m-2"
@@ -35,15 +33,15 @@ function generateButtons() {
       >
         ` + letter + `
       </button>
-    `).join('');
+    `).join("");
 
-  document.getElementById('keyboard').innerHTML = buttonsHTML;
+  document.getElementById("keyboard").innerHTML = buttonsHTML;
 }
-
+// Function that pushes the selected letter through.
 function handleGuess(chosenLetter) {
   guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
-  document.getElementById(chosenLetter).setAttribute('disabled', true);
-
+  document.getElementById(chosenLetter).setAttribute("disabled", true);
+// Checks whether the choosen letter is correct or incorect.
   if (answer.indexOf(chosenLetter) >= 0) {
     guessedWord();
     checkIfGameWon();
@@ -54,38 +52,38 @@ function handleGuess(chosenLetter) {
     updateHangmanPicture();
   }
 }
-
+// Function that checks for incorrect answers and corrasponds with the images.
 function updateHangmanPicture() {
-  document.getElementById('hangmanPic').src = './images/' + mistakes + '.jpg';
+  document.getElementById("hangmanPic").src = "./images/" + mistakes + ".png";
 }
-
+// Function that checks if the game is won.
 function checkIfGameWon() {
   if (wordStatus === answer) {
-    document.getElementById('keyboard').innerHTML = 'You Won!!!';
+    document.getElementById("keyboard").innerHTML = "You Won!";
   }
 }
-
+// Function that checks if the game is lost.
 function checkIfGameLost() {
   if (mistakes === maxWrong) {
-    document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
-    document.getElementById('keyboard').innerHTML = 'You Lost!!!';
+    document.getElementById("wordSpotlight").innerHTML = "The answer was: " + answer;
+    document.getElementById("keyboard").innerHTML = "You Lost!";
   }
 }
 
 function guessedWord() {
-  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+  wordStatus = answer.split("").map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join("");
 
-  document.getElementById('wordSpotlight').innerHTML = wordStatus;
+  document.getElementById("wordSpotlight").innerHTML = wordStatus;
 }
-
+// Function for mistakes/incorrect letters
 function updateMistakes() {
-  document.getElementById('mistakes').innerHTML = mistakes;
+  document.getElementById("mistakes").innerHTML = mistakes;
 }
-
+// This is the reset button. To restart the game once it's won or loss.
 function reset() {
   mistakes = 0;
   guessed = [];
-  document.getElementById('hangmanPic').src = './images/0.jpg';
+  document.getElementById("hangmanPic").src = "./images/0.jpg";
 
   randomWord();
   guessedWord();
@@ -93,7 +91,7 @@ function reset() {
   generateButtons();
 }
 
-document.getElementById('maxWrong').innerHTML = maxWrong;
+document.getElementById("maxWrong").innerHTML = maxWrong;
 
 randomWord();
 generateButtons();
